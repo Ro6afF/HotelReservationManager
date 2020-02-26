55c1f51c0460425b3e559217c75c28a6f6adfece
Ro6afF <dimo_chanev@windowslive.com>
2020-02-26 19:07:50 +0200

    added reservations controller

---
+++ HotelReservationManager/Controllers/ReservationController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HotelReservationManager.Data;
+using HotelReservationManager.Data.Models;
+using HotelReservationManager.Models.Reservation;
+
+namespace HotelReservationManager.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReservationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Reservation
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Reservations.ToListAsync());
+        }
+
+        // GET: Reservation/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // GET: Reservation/Create
+        public async Task<IActionResult> Create()
+        {
+            var reservationVM = new CreateReservationViewModel
+            {
+                AvaiableRooms = await _context.Rooms.Where(x => x.Free).ToListAsync(),
+                AvaiableGuests = await _context.Clients.ToListAsync()
+            };
+            return View(reservationVM);
+        }
+
+        // POST: Reservation/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CheckInTime,CheckOutTime,Breakfast,AllInclusive,Id")] CreateReservationViewModel reservationVM)
+        {
+            if (ModelState.IsValid)
+            {
+                // TODO: add creator id
+                var reservation = new Reservation
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    AllInclusive = reservationVM.AllInclusive,
+                    Breakfast = reservationVM.Breakfast,
+                    CheckInTime = reservationVM.CheckInTime,
+                    CheckOutTime = reservationVM.CheckOutTime,
+                    Guests = reservationVM.Clients,
+                    Room = reservationVM.Room,
+                };
+                _context.Add(reservation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservationVM);
+        }
+
+        // GET: Reservation/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations.FindAsync(id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+            return View(reservation);
+        }
+
+        // POST: Reservation/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CheckInTime,CheckOutTime,Breakfast,AllInclusive,Id")] Reservation reservation)
+        {
+            if (id != reservation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(reservation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ReservationExists(reservation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(reservation);
+        }
+
+        // GET: Reservation/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var reservation = await _context.Reservations
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (reservation == null)
+            {
+                return NotFound();
+            }
+
+            return View(reservation);
+        }
+
+        // POST: Reservation/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var reservation = await _context.Reservations.FindAsync(id);
+            _context.Reservations.Remove(reservation);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ReservationExists(string id)
+        {
+            return _context.Reservations.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HotelReservationManager/Models/Reservation/CreateReservationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelReservationManager.Models.Reservation
+{
+    public class CreateReservationViewModel
+    {
+        public List<Data.Models.Room> AvaiableRooms { get; set; }
+        public List<Data.Models.Client> AvaiableGuests { get; set; }
+        public Data.Models.Room Room { get; set; }
+        public string CreatorId { get; set; }
+        public List<Data.Models.Client> Clients { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime CheckOutTime { get; set; }
+        public bool Breakfast { get; set; }
+        public bool AllInclusive { get; set; }
+    }
+}
